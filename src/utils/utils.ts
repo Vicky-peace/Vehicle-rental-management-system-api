40d351692532f83da9c4ac2baf2442e2c7751734
Vicky-peace <victorpeace47@gmail.com>
2025-03-16 13:08:40 +0300

    updated the restaurent api

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,2 @@
 // export const ClientURL = "http://localhost:5173";
-export const ClientURL = "https://white-sky-095ea8f03.5.azurestaticapps.net";
+export const ClientURL = "https://vehicle-rental-management-system-api-9thn.onrender.com";
